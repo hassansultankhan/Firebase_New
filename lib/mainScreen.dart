92ba97f92f5bbe4e261c7a01dca797a5a9c39b4e
Hassan Sultan Khan <hassansultankhan@gmail.com>
2022-09-09 14:09:56 -0700

    9/9/22

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 
-
 class mainScreen extends StatefulWidget {
   const mainScreen({Key? key}) : super(key: key);
 
@@ -12,23 +11,33 @@ class mainScreen extends StatefulWidget {
 }
 
 class _mainScreenState extends State<mainScreen> {
+  late Query _ref;
+  DatabaseReference reference =
+      FirebaseDatabase.instance.ref().child('Products');
+
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _ref = FirebaseDatabase.instance.ref().child('Products').child('name');
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Products"),
         centerTitle: true,
       ),
-    
-    body: FloatingActionButton(
-      child: Icon(Icons.add),
-      onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder:(context)=> AddData(),));
-      },
-    )
-    ,
+      body: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddData(),
+              ));
+        },
+      ),
     );
-    
   }
-  
 }
